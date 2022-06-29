a1a6d90a36a8f7b9c0f263a1c9bc3cdbfe9ad8fe
Georgi Georgiev <georgi.g@codecomrades.com>
2022-06-29 11:19:06 +0300

    init push

---
+++ online-menus/src/helpers/Backend-Service.tsx
@@ -0,0 +1,15 @@
+import { SITE_CONFIG } from './site-config';
+
+export class BackendService {
+    async call(url: string, method: 'PUT' | 'POST' | 'GET', body?: Object) {
+        const requestOptions = {
+            method: method,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body),
+        };
+
+        return fetch(`${SITE_CONFIG.ENVIRONMENT_URL}${url}`, requestOptions).then((response) =>
+            response.json()
+        );
+    }
+}
---
+++ online-menus/src/helpers/site-config.tsx
@@ -0,0 +1,4 @@
+export const SITE_CONFIG = {
+    PAGE_SIZE: 5,
+    ENVIRONMENT_URL: 'https://my-json-server.typicode.com/ggeorgiev4/CC-Online-Menus',
+};
